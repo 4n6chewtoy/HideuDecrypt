d67c21bbebe0babf30a4182874b50443b09d3774
4n6chewtoy <97304946+4n6chewtoy@users.noreply.github.com>
2025-01-19 23:51:25 +0000

    Update HideUDecrypt.py

--- HideUDecrypt.py
+++ HideUDecrypt.py
@@ -14,7 +14,7 @@ hex_passphrase = "92e418a05804d6fcaa5e0a0f3729b4ee"
 # Convert the hex passphrase to raw bytes
 passphrase_bytes = bytes.fromhex(hex_passphrase)
 
-# salt - first 16 bytes of 
+# salt - first 16 bytes of encrypted DB
 salt = b'\x52\xA3\xA8\x69\x5F\x1B\x5A\x7F\xFA\xF3\x09\x8C\x49\x07\xB9\x65'
 
 # Params for Sql 4
